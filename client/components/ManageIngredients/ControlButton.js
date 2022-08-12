d2bb8d15aad3b0b185c04bb8a83e851b6385e2c6
Aikamoine <atte.laakso@helsinki.fi>
2022-08-12 15:49:25 +0300

    Improvements (toasts and id handling) and bug fixes (filtering) to ingredient view

---
+++ client/components/ManageIngredients/ControlButton.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+const ControlButton = ({
+  ingredient,
+  handleRemove,
+  handleReplace,
+  handleDelete,
+  handleAddIngredient,
+}) => {
+  if (ingredient.id <= 0) {
+    return (
+      <>
+        <Button variant="danger" size="sm" onClick={(event) => handleRemove(event, ingredient.id)}>Peru lisäys</Button>
+        {'    '}
+        { ingredient.name && <Button size="sm" onClick={(event) => handleAddIngredient(event, ingredient)}>Lisää ainesosa</Button>}
+      </>
+    )
+  }
+
+  if (!ingredient.name) {
+    return null
+  }
+
+  if (Number(ingredient.count) === 0 && !ingredient.edited) {
+    return (
+      <Button size="sm" onClick={(event) => handleDelete(event, ingredient)}>Poista kokonaan</Button>
+    )
+  }
+
+  if (!ingredient.edited) {
+    return null
+  }
+
+  if (Number(ingredient.count) === 0) {
+    return (
+      <>
+        <Button variant="warning" size="sm" onClick={(event) => handleReplace(event, ingredient)}>Korvaa nykyisellä tekstillä</Button>
+        <Button variant="danger" size="sm" onClick={(event) => handleDelete(event, ingredient)}>Poista kokonaan</Button>
+      </>
+    )
+  }
+  return (
+    <Button variant="warning" size="sm" onClick={(event) => handleReplace(event, ingredient)}>Korvaa nykyisellä tekstillä</Button>
+  )
+}
+
+export default ControlButton
--- client/components/ManageIngredients/index.js
+++ client/components/ManageIngredients/index.js
@@ -13,58 +13,15 @@ import {
   replaceIngredientName,
   deleteIngredient,
 } from 'Utilities/services/ingredients'
+import ControlButton from './ControlButton'
 
 import foodNames from '../../assets/foodNames.json'
-
-const ControlButton = ({
-  index,
-  ingredient,
-  handleRemove,
-  handleReplace,
-  handleDelete,
-  handleAddIngredient,
-}) => {
-  if (!ingredient.name) {
-    return null
-  }
-
-  if (Number(ingredient.count) === 0 && !ingredient.edited) {
-    return (
-      <Button size="sm" onClick={(event) => handleDelete(event, ingredient)}>Poista kokonaan</Button>
-    )
-  }
-
-  if (!ingredient.edited) {
-    return null
-  }
-
-  if (ingredient.id <= 0) {
-    return (
-      <>
-        <Button variant="danger" size="sm" onClick={(event) => handleRemove(event, index)}>Peru lisäys</Button>
-        {'    '}
-        <Button size="sm" onClick={(event) => handleAddIngredient(event, ingredient)}>Lisää ainesosa</Button>
-      </>
-    )
-  }
-
-  if (Number(ingredient.count) === 0) {
-    return (
-      <>
-        <Button variant="warning" size="sm" onClick={(event) => handleReplace(event, ingredient)}>Korvaa nykyisellä tekstillä</Button>
-        <Button variant="danger" size="sm" onClick={(event) => handleDelete(event, index)}>Poista kokonaan</Button>
-      </>
-    )
-  }
-  return (
-    <Button variant="warning" size="sm" onClick={(event) => handleReplace(event, ingredient)}>Korvaa nykyisellä tekstillä</Button>
-  )
-}
 
 const ManageIngredients = () => {
   const [ingredientList, setIngredientList] = useState([])
   const [fineliIngredients, setFineliIngredients] = useState()
   const [isAdmin, setIsAdmin] = useState(false)
+  const [filter, setFilter] = useState('')
 
   const handleGetIngredients = async () => {
     const admin = await userIsAdmin()
@@ -73,6 +30,7 @@ const ManageIngredients = () => {
     setIngredientList(allIngredients)
     const fineliNames = foodNames.map((food) => ({ value: food.FOODID, label: `${food.FOODNAME}, ${food.FOODTYPE}` }))
     setFineliIngredients(fineliNames)
+    setFilter('')
   }
 
   useEffect(() => {
@@ -90,24 +48,24 @@ const ManageIngredients = () => {
     }, ...ingredientList])
   }
 
-  const handleChange = (event, index) => {
+  const handleChange = (event, id) => {
     const { name, value } = event.target
-    const list = [...ingredientList]
-    list[index][name] = value
-    list[index].edited = true
-    setIngredientList(list)
+    const element = ingredientList.filter((i) => i.id === id)[0]
+    element[name] = value
+    element.edited = true
+    setIngredientList([...ingredientList])
   }
 
-  const handleRemove = (event, index) => {
-    const filteredArray = ingredientList.filter((ing, i) => i !== index)
+  const handleRemove = (event, id) => {
+    const filteredArray = ingredientList.filter((i) => i.id !== id)
     setIngredientList(filteredArray)
   }
 
   const handleReplace = async (event, ingredient) => {
     // eslint-disable-next-line no-alert
     if (window.confirm(`Hyväksymällä kaikki kannassa ID:llä ${ingredient.id} olevat ainesosat korvataan nimellä ${ingredient.name}`)) {
+      toast('Korvataan...')
       const replaced = await replaceIngredientName(ingredient)
-      // toast(`Korvattu '${replaced.originalName}' arvolla '${replaced.newName}' kaikissa resepteissä`)
       toast(replaced.message)
       handleGetIngredients()
     }
@@ -134,8 +92,10 @@ const ManageIngredients = () => {
 
   const handleAddIngredient = async (event, ingredient) => {
     console.log('adding', ingredient)
+    toast(`Lisätään ainesosaa ${ingredient.name}`)
     const added = await addIngredient(ingredient)
-    console.log('added', added)
+    toast(`Lisätty ${added.name}`)
+    handleGetIngredients()
   }
 
   if (!isAdmin.isAdmin) {
@@ -145,6 +105,8 @@ const ManageIngredients = () => {
       </div>
     )
   }
+
+  const filtered = ingredientList.filter((i) => i.name.includes(filter.toLowerCase()) || i.edited)
 
   return (
     <div>
@@ -164,14 +126,19 @@ const ManageIngredients = () => {
             </th>
             <th>Käyttökertoja</th>
           </tr>
+          <tr>
+            <td>
+              suodatus: <input value={filter} onChange={({ target }) => setFilter(target.value)} />
+            </td>
+            <td />
+          </tr>
         </thead>
         <tbody>
-          {ingredientList.map((ingredient, index) => (
+          {filtered.map((ingredient) => (
             <tr key={ingredient.id}>
               <td>
-                <input value={ingredient.name} name="name" onChange={(event) => handleChange(event, index)} />
+                <input value={ingredient.name} name="name" onChange={(event) => handleChange(event, ingredient.id)} />
                 <ControlButton
-                  index={index}
                   ingredient={ingredient}
                   handleRemove={handleRemove}
                   handleReplace={handleReplace}
