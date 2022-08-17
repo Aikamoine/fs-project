3ff71253b176ad4bb42d192b7c779525ed61d357
Aikamoine <atte.laakso@helsinki.fi>
2022-08-17 19:45:22 +0300

    footer update

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => (
   <div className="footer">
-    Full Stack -websovelluskehitys harjoitustyö
+    <div>
+      Full Stack -websovelluskehitys harjoitustyö
+    </div>
+    <div>
+      Ravintotiedoissa on osin käytetty Terveyden ja hyvinvoinnin laitoksen (Fineli) avointa dataa (CC-BY)
+    </div>
   </div>
 )
 
