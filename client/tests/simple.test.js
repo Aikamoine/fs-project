9e5d0da4698cdc49eb10a57dc8f439fb8f649275
Aikamoine <atte.laakso@helsinki.fi>
2022-08-19 15:09:08 +0300

    Revert "fixed broken dependency"
    This reverts commit 47fc55cdd10ea2daefc4de0d3f48c0734854f406.

--- client/tests/simple.test.js
+++ client/tests/simple.test.js
@@ -1,33 +1,3 @@
-/*
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import RecipeList from '../components/RecipeView/RecipeList'
-
-*/
-
-test('works', () => {
+it('works', () => {
   expect(1 + 1).toBe(2)
 })
-/*
-const recipes = [
-  {
-    id: 1,
-    info: 'infotext',
-    name: 'first recipe',
-    servings: 4,
-    time: '15 min',
-  },
-  {
-    id: 2,
-    name: 'second recipe',
-    servings: 0,
-    time: '30 min',
-  },
-]
-
-it('recipelist renders list of recipes', () => {
-  render(<RecipeList recipes={recipes} />)
-  const element = screen.getByText('first recipe')
-  expect(element).toBeDefined()
-})
-*/
