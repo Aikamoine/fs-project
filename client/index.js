adcbd9a3b15c4c54332cb5f62bbdc1f91c14225d
Aikamoine <atte.laakso@helsinki.fi>
2022-08-19 16:37:05 +0300

    revert to render(...

--- client/index.js
+++ client/index.js
@@ -1,27 +1,16 @@
 import React from 'react'
-// import { render } from 'react-dom'
-import ReactDOM from 'react-dom/client'
+import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import 'Assets/custom.scss'
 import App from 'Components/App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-const refresh = () => root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
-
-/*
 const refresh = () => render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
 )
-*/
 
 refresh()
 
